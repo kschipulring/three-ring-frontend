7aaa040b6d80e8bda2baa5da94c87d3cf88d3b32
kschipulring <karl@3ringprototype.com>
2020-04-09 22:51:39 -0400

    all submit input elements are disabled by default

--- src/ReactiveForm/InputTransform.js
+++ src/ReactiveForm/InputTransform.js
@@ -3,12 +3,15 @@ import React from 'react';
 function InputTransform(node, k, f=null){
   let attrs = node.attribs;  console.log( {f} );
 
+  //we want the submit button to be disabled by default.  Avoids spammy stuff.
+  let if_disabled = (attrs.type === "submit");
+
   attrs.className = attrs.class;
 
   delete attrs.class;
 
   return <input type={attrs.type} className={attrs.className}
-  name={attrs.name} key={k} onChange={f} />;
+  name={attrs.name} key={k} onChange={f} disabled={if_disabled} />;
 }
 
 export default InputTransform;
