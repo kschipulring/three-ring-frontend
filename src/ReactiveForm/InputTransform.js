7bb5c48ba10217001c172f8d459c4d40969e611a
kschipulring <karl@3ringprototype.com>
2020-04-11 13:50:35 -0400

    disabling submit button for intiial onload and for form errors

--- src/ReactiveForm/InputTransform.js
+++ src/ReactiveForm/InputTransform.js
@@ -4,13 +4,14 @@ function InputTransform(node, k, f=null){
   let attrs = node.attribs;  console.log( {f} );
 
   //we want the submit button to be disabled by default.  Avoids spammy stuff.
-  let if_disabled = (attrs.type === "submit");
+  let if_disabled = (attrs.type === "submit") && !(attrs.form_enabled);
+  let if_title = if_disabled ? "please complete the form" : "";
 
   attrs.className = attrs.class;
 
   delete attrs.class;
 
-  return <input type={attrs.type} className={attrs.className}
+  return <input type={attrs.type} className={attrs.className} title={if_title}
   name={attrs.name} key={k} onChange={f} disabled={if_disabled} />;
 }
 
