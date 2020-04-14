1e625fd35d30a0384da05af60c4d4753f7c22211
kschipulring <karl@3ringprototype.com>
2020-04-14 19:51:51 -0400

    updated TextArea component

--- src/ReactiveForm/TextAreaTransform.js
+++ src/ReactiveForm/TextAreaTransform.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-function TextAreaTransform(node, k, f=null){
-  let attrs = node.attribs;
-
+function TextAreaTransform({attrs, k, f}) {
+  //React likes this property, not the one it is being copied from.
   attrs.className = attrs.class;
 
+  //React is cranky with this perfectly good, normal javascript attribute
   delete attrs.class;
 
-  return ( <textarea {...attrs} key={k} onChange={f}></textarea> );
+  let required = attrs['aria-required'] ? true : false;
+
+  return (
+    <textarea {...attrs} key={k} onChange={f} required={required}></textarea>
+  );
 }
 
 export default TextAreaTransform;
