b8c553897adaa443bc8fa60c0e8afd2f22185afb
kschipulring <karl@3ringprototype.com>
2020-04-14 19:40:29 -0400

    updated Select fields component

--- src/ReactiveForm/SelectTransform.js
+++ src/ReactiveForm/SelectTransform.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
-function SelectTransform(node, k, f=null){
+function SelectTransform({node, k, f}){
   let attrs = node.attribs;
 
   attrs.className = attrs.class;
-
   delete attrs.class;
 
-  console.log( node.children );
+  /* React prefers the 'defaultValue' attribute instead of applying a 'selected'
+  value to an individual option. So first find the single option array member, then... */
+  let selected_option = node.children.filter(node => node.attribs.selected);
 
-  return <select className={attrs.className} key={k} {...attrs} onChange={f} >
+  //string value of above
+  let sel_val = selected_option[0].attribs.value;
+  
+  let required = attrs['aria-required'] ? true : false;
+
+  return <select key={k} {...attrs} onChange={f} defaultValue={sel_val} 
+    required={required}>
   {
     node.children.map(
-      (item, k2) => <option key={k2}>
+      (item, k2) => <option key={k2} value={item.attribs.value} >
       {
         item.data || item.children.map( i => i.data || "" )
       }
