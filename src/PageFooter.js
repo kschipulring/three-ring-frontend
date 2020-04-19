f86015e80db6557a23104b032a6b801cb52d9f5a
kschipulring <karl@3ringprototype.com>
2020-04-19 07:52:50 -0400

    footer tweaks

--- src/PageFooter.js
+++ src/PageFooter.js
@@ -5,18 +5,15 @@ import NavBar from './NavBar';
 function PageFooter(props){
   let nav_items = props.items;
 
-  console.log( {nav_items} );
-
   return (
-      <>
-        <footer id="/footer/">
-          <NavBar id="footer_nav" items={nav_items} />
+    <footer id="/footer/">
+      <h2>Three Ring Design</h2>
+      <NavBar id="footer_nav" items={nav_items} />
 
-          <p>Stamford, CT</p>
-          <p>Clemson, SC</p>
-        </footer>
-      </>
-    );
+      <p>Stamford, CT</p>
+      <p>Clemson, SC</p>
+    </footer>
+  );
 }
 
 export default PageFooter;
