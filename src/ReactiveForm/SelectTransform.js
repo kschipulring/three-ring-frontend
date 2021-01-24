2f57e77755780826f6e1c5a95fc674b159413ab2
kschipulring <karl@3ringprototype.com>
2021-01-24 15:43:23 -0500

    SelectTransform bugfixes for when a select box or its children may not have all the right attributes.

--- src/ReactiveForm/SelectTransform.js
+++ src/ReactiveForm/SelectTransform.js
@@ -8,22 +8,28 @@ function SelectTransform({node, k, f}){
 
   /* React prefers the 'defaultValue' attribute instead of applying a 'selected'
   value to an individual option. So first find the single option array member, then... */
-  let selected_option = node.children.filter(node => node.attribs.selected);
+  let [selected_option] = node.children.filter(node => node.attribs && node.attribs.selected );
 
   //string value of above
-  let sel_val = selected_option[0].attribs.value;
-  
+  let sel_val = selected_option ? selected_option.attribs.value : "";
+
   let required = attrs['aria-required'] ? true : false;
 
   return <select key={k} {...attrs} onChange={f} defaultValue={sel_val} 
     required={required}>
   {
     node.children.map(
-      (item, k2) => <option key={k2} value={item.attribs.value} >
-      {
-        item.data || item.children.map( i => i.data || "" )
+      (item, k2) => {
+        if(item && item.attribs && item.attribs.value){
+          return <option key={k2} value={ item.attribs.value } >
+            {
+              item.data || item.children.map( i => i.data || "" )
+            }
+          </option>
+        }else{
+          return "";
+        }
       }
-      </option>
     )
   }
   </select>;
